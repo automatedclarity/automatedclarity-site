0ccda1bc9e06e9137d0b7ca06e956daed821ba87
automatedclarity <support@automatedclarity.com>
2025-11-10 18:42:52 -0800

    Update auth-check.js

--- functions/auth-check.js
+++ functions/auth-check.js
@@ -1,4 +1,6 @@
+// functions/auth-check.js
 import { readSession } from "./_lib/session.js";
+
 export default async (req) => {
   const sess = readSession(req);
   if (!sess) return new Response(JSON.stringify({ ok:false }), { status:401 });
